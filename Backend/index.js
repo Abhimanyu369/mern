832c2e3587c84d969908377aecc0a71d5015564b
Abhimanyu Malik <abhimanyumalik369@gmail.com>
2021-02-11 01:51:52 +0530

    Server Setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,3 +1,11 @@
 const express = require("express")
+const app = express()
+const port = 8000
 
-const app = express();
+app.get("/", (req, res)=>{
+    return res.send("hello world")
+})
+
+app.listen(port, ()=>{
+    console.log("Server is up and running...")
+})

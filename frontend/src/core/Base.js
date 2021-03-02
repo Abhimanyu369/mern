1915f470bf62cb65d31a9d517c2e263ffec27493
Abhimanyu Malik <abhimanyumalik369@gmail.com>
2021-03-02 21:42:59 +0530

    update base component

--- frontend/src/core/Base.js
+++ frontend/src/core/Base.js
@@ -1,10 +1,23 @@
 import React from 'react';
-const Base = () => {
+const Base = ({children}) => {
     return ( 
         <div>
             <div className="container-fluid">
-                
+                <div className="jumbotron bg-dark text-white text-center">
+                    <h2 className="display-4">My Title</h2>
+                    <p className="lead">My Description</p>
+                </div>
+                <p>{children}</p>
             </div>
+            <footer className="footer bg-dark mt-auto py-3">
+                <div className="container-fluid">
+                    Some Text
+                    <button className="btn btn-warning">Contact Us</button>
+                </div>
+                <div className="container">
+                    <span className="text-muted">Some text</span>
+                </div>
+            </footer>   
         </div>
     );
 }
--- frontend/src/core/Home.js
+++ frontend/src/core/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import {API} from "../backend"
+import {API} from "../backend";
+import Base from "./Base";
 
 const Home = () => {
     console.log("Api: ", API)
     return ( 
-        <div>
-            Home
-        </div>
+        <Base>
+            <div>
+                Home
+            </div>
+        </Base>
     );
 }
  
